a8bb29b830c1911dd5e01f8bf456c9e47183ba0e
Francis Marchitelli <127345349+francismarchitelli@users.noreply.github.com>
2023-10-24 13:05:46 -0400

    Update reservations.router.js
    Changing the routes order

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -8,10 +8,10 @@ const router = require("express").Router();
 const controller = require("./reservations.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").post(controller.create).get(controller.list).all(methodNotAllowed);
+router.route("/:reservation_id/status").put(controller.updateReservationStatus).all(methodNotAllowed);
 
 router.route("/:reservation_id").get(controller.read).put(controller.update).all(methodNotAllowed);
 
-router.route("/:reservation_id/status").put(controller.updateReservationStatus).all(methodNotAllowed);
+router.route("/").post(controller.create).get(controller.list).all(methodNotAllowed);
 
 module.exports = router;
