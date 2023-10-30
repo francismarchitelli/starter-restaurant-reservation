06dffe98bbccc0a5e0da37f99154ea484e47f032
Francis Marchitelli <127345349+francismarchitelli@users.noreply.github.com>
2023-10-30 10:29:14 -0400

    Adding the reservations files

---
+++ front-end/src/reservations/NewReservation.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { createReservation } from "../utils/api";
+import ReservationForm from "./ReservationForm";
+
+export const NewReservation = () => {
+  const history = useHistory();
+  const initialState = {
+    first_name: "",
+    last_name: "",
+    mobile_number: "",
+    reservation_date: "",
+    reservation_time: "",
+    people: 0,
+  };
+
+  const [reservation, setReservation] = useState({...initialState,});
+
+  const handleReservationChange = (e) => {
+    if (e.target.name === "people") {
+      setReservation({...reservation, [e.target.name]: Number(e.target.value),
+      });
+    } else {
+      setReservation({...reservation, [e.target.name]: e.target.value,
+      });
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const abortController = new AbortController();
+
+    try {
+      await createReservation(reservation, abortController.signal);
+      history.push(`/dashboard?date=${reservation.reservation_date}`);
+    } catch (error) {
+        return error;
+    }
+
+    return () => abortController.abort();
+  };
+
+  return (
+    <section>
+      <h1>Create a Reservation</h1>
+      <ReservationForm
+        reservation={reservation}
+        changeHandler={handleReservationChange}
+        submitHandler={handleSubmit}
+      />
+    </section>
+  );
+};
+
+export default NewReservation;
