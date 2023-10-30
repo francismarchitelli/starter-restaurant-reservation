8b0ab60966c2ede0468e46e5cea35d2922f84621
Francis Marchitelli <127345349+francismarchitelli@users.noreply.github.com>
2023-10-30 10:30:49 -0400

    Update Routes.js

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -4,6 +4,11 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
+import NewReservation from "../reservations/NewReservation";
+import SearchReservation from "../reservations/SearchReservation";
+import EditReservation from "../reservations/EditReservation";
+import SeatReservation from "../reservations/SeatReservation";
+import NewTable from "../tables/NewTable";
 
 /**
  * Defines all the routes for the application.
@@ -18,12 +23,35 @@ function Routes() {
       <Route exact={true} path="/">
         <Redirect to={"/dashboard"} />
       </Route>
+
       <Route exact={true} path="/reservations">
         <Redirect to={"/dashboard"} />
       </Route>
+
       <Route path="/dashboard">
         <Dashboard date={today()} />
       </Route>
+      
+      <Route path="/reservations/new">
+        <NewReservation />
+      </Route>
+
+      <Route path="/reservations/:reservation_id/edit">
+        <EditReservation />
+      </Route>
+
+      <Route path="/reservations/:reservation_id/seat">
+        <SeatReservation />
+      </Route>
+
+      <Route path="/search">
+        <SearchReservation />
+      </Route>
+
+      <Route path="/tables/new">
+        <NewTable />
+      </Route>
+
       <Route>
         <NotFound />
       </Route>
